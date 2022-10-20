c839c5df6909ad57511177a7fd90218186f4a08a
Estebanm20 <esteban.medina@ucsp.edu.pe>
2022-10-20 14:47:22 -0500

    Avance2

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,9 +3,47 @@
 #include "Usuario.h"
 using namespace std;
 
-int main(){
-    Usuario Usuario1 {"Montalvan","Samuel","Administración de narcotrafico","1234567"};
-    cout << Usuario1.getname() << endl;
 
+
+void elegir(int n){
+    switch (n)
+    {
+    case 1:
+        cout << "Administrador" << endl;
+
+        break;
+    case 2:
+        cout << "Profesor" << endl;
+        break;
+    case 3:
+        cout << "Alumno" << endl;
+        break;
+    default:
+        cout << "=============================\n\tHasta Pronto\n=============================" << endl;
+        break;
+    }
+}
+
+
+void menu(){
+    cout << "Bienvenidos a la UCSP" << endl;
+    cout << "========================" << endl;
+    cout << "Elija entre algunas de estas opciones: "<< endl;
+    cout << "========================" << endl;
+    cout << "[1] Administrador\n[2] Profesor\n[3] Alumno\n[0] Salir." << endl;
+    int n;
+
+    do {
+        cin >> n;
+        elegir(n);
+    }
+    while(n);
+
+}
+
+int main(){
+    Usuario Usuario1 {"Montalvan","Samuel","Panadero nuclear","1234567"};
+    cout << Usuario1.getname() << endl;
+    menu();
     return 0;
 }
