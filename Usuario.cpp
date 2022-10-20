4d85951b19ab7df0f4ca3025618adfe0ba7b9ca3
Estebanm20 <esteban.medina@ucsp.edu.pe>
2022-10-20 14:19:03 -0500

    Avance

--- Usuario.cpp
+++ Usuario.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    Usuario Usuario1 {"Samuel","1234567"};
+    Usuario Usuario1 {"Montalvan","Samuel","Administración de narcotrafico","1234567"};
     cout << Usuario1.getname() << endl;
 
     return 0;
--- Usuario.h
+++ Usuario.h
@@ -4,13 +4,19 @@ using namespace std;
 
 class Usuario{
     public: 
-    Usuario (string _Username, string _Pasword): Username{_Username} ,Pasword{_Pasword} {}
+    Usuario (string _Username, string _Pasword, string _Apellido, string _Carrera): Username{_Username} ,Pasword{_Pasword}, Apellido{_Apellido}, Carrera{_Carrera} {}
     //set
     void setname(string _Username){
         Username = _Username;
     }
     void setpasword(string _Pasword){
         Pasword = _Pasword;
+    }
+    void setApellido(string _Apellido){
+        Apellido = _Apellido;
+    }
+    void setCarrera(string _Carrera){
+        Carrera = _Carrera;
     }
     //get
     string getname()const{
@@ -19,8 +25,16 @@ class Usuario{
     string getpasword()const{
         return Pasword;
     }
+    string getApellido() const {
+        return Apellido;
+    }
+    string getApelli() const {
+        return Carrera;
+    }
 
     private:
     string Username;
     string Pasword;
+    string Apellido;
+    string Carrera;
 };
